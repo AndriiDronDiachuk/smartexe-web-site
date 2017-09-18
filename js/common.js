fd6bf0e1e6c9d5f80e1e030c548d225dbbb778db
Vlad Revenko <vladiknorik@gmail.com>
2017-09-18 08:41:01 +0300

    v 2.1.0 Fixed pages for desktops and large laptops. Added adaptive for laptops, netbooks and tablets.

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $(document).ready(function($) {
-    $('.carousel').unslider();
+    $('.carousel').unslider({
+        autoplay: true,
+        speed: 1000,
+        delay: 10000
+    });
+
+    $('.marquee').marquee({
+        duration: 25000,
+        gap: 1,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });
+
+    $('#nav-bar').click(function() {
+        $('#nav-menu').slideToggle( "slow" );
+    });
 });
