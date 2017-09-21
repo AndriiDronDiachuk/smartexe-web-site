f6b9f72726f22b6d57dd02c7d7f32215703602cb
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-09-21 15:47:46 +0300

    V 3.3.7 Fixed "contact us" mobile style, fixed map.

--- js/map.js
+++ js/map.js
@@ -2,13 +2,19 @@ function initMap() {
     var uluru = {lat: 32.0940677, lng: 34.82323829999996};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 17,
-        center: uluru
+        center: uluru,
+        disableDefaultUI: true,
+        draggable: false,
+        zoomControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true
     });
     var marker = new google.maps.Marker({
+        clickable: true,
         position: new google.maps.LatLng(32.0940677,34.82323829999996),
         map: map
     });
-    /*marker.setIcon('images/logo.svg');*/
+    marker.setIcon({url:'images/map_marker.svg', size: new google.maps.Size(55, 70)});
     var contentString = '<div class="marker-content">'+
         '<h3>ISRAEL</h3>'+
         '<p>23, Bar Kochva st., 7th Floor, Bney Brak, Israel</p>'+
