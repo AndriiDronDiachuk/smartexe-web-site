6495c24b2bb1dbd7da6af86314fa438684716244
Vlad Revenko <vladiknorik@gmail.com>
2017-09-21 16:39:20 +0300

    v 3.3.14 Fixed "Home" page.

--- js/common.js
+++ js/common.js
@@ -1,21 +1,5 @@
 $(document).ready(function($) {
 
-    if (window.location.pathname.includes('index') || window.location.pathname === '/smartexe-web-site/') {
-        $('.carousel').unslider({
-            autoplay: true,
-            speed: 1000,
-            delay: 10000
-        });
-
-        $('.marquee').marquee({
-            duration: 25000,
-            gap: 1,
-            delayBeforeStart: 0,
-            direction: 'left',
-            duplicated: true
-        });
-    }
-
     $('#nav-bar').click(function() {
         $('#nav-menu').toggle( 'slide', {direction: 'right'}, 250 );
         $('body').toggleClass('body-shadow');
---
+++ js/home.js
@@ -0,0 +1,16 @@
+$(document).ready(function ($) {
+
+    $('.carousel').unslider({
+        autoplay: true,
+        speed: 1000,
+        delay: 10000
+    });
+
+    $('.marquee').marquee({
+        duration: 25000,
+        gap: 1,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });
+});
