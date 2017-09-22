3f482e940b86e181f4ef39e64bb15a899492d2e9
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-09-22 15:43:21 +0300

    V 3.4.10 Fixed style for "index".

--- js/home.js
+++ js/home.js
@@ -16,7 +16,7 @@ $(document).ready(function ($) {
 
     var homePageId = ['#home-product-management', '#home-project-management', '#home-development', '#home-graphic-design', '#home-cloud-it', '#home-quality-assurance'];
 
-    homePageId.forEach(item => {
+    homePageId.forEach(function(item)  {
         $(item).on('click', function () {
             $(item).toggleClass('active');
             $( item + '-text').slideToggle();
