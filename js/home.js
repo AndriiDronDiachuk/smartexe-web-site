c0b41d4f7e3614bc552226191f342e0365a2915d
Vlad Revenko <vladiknorik@gmail.com>
2017-09-22 10:37:29 +0300

    v 3.4.0 Update "Home" page.

--- js/home.js
+++ js/home.js
@@ -13,4 +13,13 @@ $(document).ready(function ($) {
         direction: 'left',
         duplicated: true
     });
+
+    var homePageId = ['#home-product-management', '#home-project-management', '#home-development', '#home-graphic-design', '#home-cloud-it', '#home-quality-assurance'];
+
+    homePageId.forEach(item => {
+        $(item).on('click', function () {
+            $(item).toggleClass('active');
+            $( item + '-text').slideToggle();
+        });
+    });
 });
