c7d8cdac44729fa32a461c8dcaa97605eef81b9a
Vlad Revenko <vladiknorik@gmail.com>
2017-10-02 16:07:49 +0300

    v 3.8.13 Added increment effect to numbers. Fix footer

--- js/home.js
+++ js/home.js
@@ -28,4 +28,34 @@ $(document).ready(function ($) {
             $( item + '-text').slideToggle();
         });
     });
+
+    var numbers = document.querySelector('.layer-5'),
+        numberTop = numbers.getBoundingClientRect().top;
+
+    window.addEventListener('scroll', function onScroll() {
+        if(window.pageYOffset > numberTop - window.innerHeight * 0.5) {
+            this.removeEventListener('scroll', onScroll);
+
+            $("#spincrement-project-completed").spincrement({
+                from: 0,
+                to: 500,
+                duration: 2000,
+                leeway: 1
+            });
+
+            $("#spincrement-years-of-experience").spincrement({
+                from: 0,
+                to: 13,
+                duration: 2000,
+                leeway: 1
+            });
+
+            $("#spincrement-clients").spincrement({
+                from: 0,
+                to: 184,
+                duration: 2000,
+                leeway: 1
+            });
+        }
+    });
 });
