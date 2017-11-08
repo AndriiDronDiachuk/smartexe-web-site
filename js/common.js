4f7655f078031ecccf0d1261461241d9f4ac0549
Vlad Revenko <vladiknorik@gmail.com>
2017-11-08 16:31:11 +0200

    v 3.11.4 Try to get location

--- js/common.js
+++ js/common.js
@@ -12,30 +12,30 @@ $(document).ready(function($) {
         $('header').toggleClass('header-shadow');
     });
 
-    $.getJSON("https://ip-api.com/json/?fields=timezone,countryCode", function (data) {
-        var phoneNumber;
-
-        try {
-            var countryCode = data.countryCode,
-                timezone = data.timezone.split('/')[0];
-
-            if (timezone === 'Europe' && countryCode === 'IL') {
-                phoneNumber = 'Call Us: +972-3-6133886'; // Israel
-            }
-            else if (timezone === 'America') {
-                phoneNumber = 'Call Us: +1 (215) 948-8178'; // America
-            }
-            else {
-                phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
-            }
-
-        } catch (err) {
-            phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
-        }
-
-        $('#phoneNumber').text(phoneNumber);
-    })
-        .fail(function (err) {
-            $('#phoneNumber').text('Call Us: +45-30-48-56-40'); // Europe
-        });
+    // $.getJSON("https://ip-api.com/json/?fields=timezone,countryCode", function (data) {
+    //     var phoneNumber;
+    //
+    //     try {
+    //         var countryCode = data.countryCode,
+    //             timezone = data.timezone.split('/')[0];
+    //
+    //         if (timezone === 'Europe' && countryCode === 'IL') {
+    //             phoneNumber = 'Call Us: +972-3-6133886'; // Israel
+    //         }
+    //         else if (timezone === 'America') {
+    //             phoneNumber = 'Call Us: +1 (215) 948-8178'; // America
+    //         }
+    //         else {
+    //             phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
+    //         }
+    //
+    //     } catch (err) {
+    //         phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
+    //     }
+    //
+    //     $('#phoneNumber').text(phoneNumber);
+    // })
+    //     .fail(function (err) {
+    //         $('#phoneNumber').text('Call Us: +45-30-48-56-40'); // Europe
+    //     });
 });
