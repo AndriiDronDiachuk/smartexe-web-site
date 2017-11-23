5d7ce6bd0fedd5d1299960cbf0c0117294c2bc34
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-11-23 15:47:29 +0200

    v 3.11.21 Fixed clients carousel style error on "Client" page.

--- js/clients.js
+++ js/clients.js
@@ -1,6 +1,8 @@
 $(document).ready(function ($) {
 
-    $('.client-carousel').unslider({
+    $('.client-carousel')
+        .css("opacity", "1")
+        .unslider({
         autoplay: true,
         speed: 1000,
         delay: 10000
