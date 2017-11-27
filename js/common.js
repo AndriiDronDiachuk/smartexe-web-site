867db98cad77addef9d9bbe176f6fbcafbd69420
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-11-27 12:17:52 +0200

    v 3.11.24 Changed geolocation service from Yandex to GeoNames in map.js v4

--- js/common.js
+++ js/common.js
@@ -15,7 +15,7 @@ $(document).ready(function($) {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
 
-            $.getJSON('http://ws.geonames.org/countryCode', {
+            $.getJSON('https://ws.geonames.org/countryCode', {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
                 type: 'JSON',
