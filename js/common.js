446fb3dc99099e256d2399f8d882e1ed118af446
AndriiDronDiachuk <dyachuk.droid@gmail.com>
2017-11-27 11:58:17 +0200

    v 3.11.22 Changed geolocation service from Yandex to GeoNames.

--- js/common.js
+++ js/common.js
@@ -12,20 +12,30 @@ $(document).ready(function($) {
         $('header').toggleClass('header-shadow');
     });
 
-    ymaps.ready(function () {
-        var country = ymaps.geolocation.country,
-            phoneNumber;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
 
-        if (country === 'Israel') {
-            phoneNumber = 'Call Us: +972-3-6133886'; // Israel
-        }
-        else if (country === 'United States' || country === 'Canada') {
-            phoneNumber = 'Call Us: +1 (215) 948-8178'; // America or Canada
-        }
-        else {
-            phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
-        }
+            $.getJSON('http://ws.geonames.org/countryCode', {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                type: 'JSON',
+                username: 'smartexeDron'
+            }, function (result) {
 
-        $('#phoneNumber').text(phoneNumber);
-    });
+                var country = result.countryName, phoneNumber;
+
+                if (country === 'Israel') {
+                    phoneNumber = 'Call Us: +972-3-6133886'; // Israel
+                }
+                else if (country === 'United States' || country === 'Canada') {
+                    phoneNumber = 'Call Us: +1 (215) 948-8178'; // America or Canada
+                }
+                else {
+                    phoneNumber = 'Call Us: +45-30-48-56-40'; // Europe
+                }
+
+                $('#phoneNumber').text(phoneNumber);
+            });
+        });
+    }
 });
