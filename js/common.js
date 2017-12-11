4251c77f01c92dd4adba1e75e931a6365b29b439
VladRevenko <vladiknorik@gmail.com>
2017-12-11 14:59:38 +0200

    v 3.11.59 Updated footer email setting

--- js/common.js
+++ js/common.js
@@ -44,24 +44,29 @@ $(document).ready(function($) {
     $.getJSON('https://freegeoip.net/json/',
         function (data) {
             var  phoneNumber = '<span>Call Us: </span>';
+            var  phoneNumberAndEmail = '<span>Call Us: </span>';
 
             if (data !== undefined) {
                 if (data.country_code === 'IL') {
                     phoneNumber += '+972-3-6133886'; // Israel
+                    phoneNumberAndEmail += '<p>+972-3-6133886</p><p>info@smartexe.com</p>'; // Israel
 
                 }
                 else if (data.country_code === 'US' || data.country_code === 'CA') {
                     phoneNumber += '+1 (215) 948-8178'; // America or Canada
+                    phoneNumberAndEmail += '<p>+1 (215) 948-8178</p><p>info@smartexe.com</p>'; // America or Canada
                 }
                 else {
                     phoneNumber += '+45-30-48-56-40'; // Europe
+                    phoneNumberAndEmail += '<p>+45-30-48-56-40</p><p>contacteu@smartexe.com</p>'; // Europe
                 }
             }
             else {
                 phoneNumber += '+45-30-48-56-40';
+                phoneNumberAndEmail += '<p>+45-30-48-56-40</p><p>contacteu@smartexe.com</p>';
             }
 
-            $('#phoneNumber').html(phoneNumber);
+            $('#phoneNumber').html(phoneNumberAndEmail);
             $('#header-phone')
                 .html(phoneNumber)
                 .css('opacity', 1);
